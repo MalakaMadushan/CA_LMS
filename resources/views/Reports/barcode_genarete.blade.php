69d0f17beb3b949c7577c7667359e7eb3078ee3f
shanukaGIT <shanuka.pvt@gmail.com>
2020-01-06 16:49:57 +0530

    barcode

--- resources/views/Reports/barcode_genarete.blade.php
+++ resources/views/Reports/barcode_genarete.blade.php
@@ -1,12 +1,14 @@
 
 <div class="container">
-<table style="border: 1px solid black;text-align:center;">
-  @for($j='10000'; $j<'12000'; $j++)
-  <tr style="margin-top: 5px;">
-  <td style="border: 1px solid black;">&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
-  <td style="border: 1px solid black;">&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
-  <td style="border: 1px solid black;">&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
-  <td style="border: 1px solid black;">&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
+<!-- <table style="border: 1px solid black;text-align:center;"> -->
+<table>
+  @for($j='10000'; $j<'10500'; $j++)
+  <tr>
+  <td ><br>&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
+  <td ><br>&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
+  <td ><br>&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
+  <td ><br>&nbsp;&nbsp;{!!DNS1D::getBarcodeSVG("BKPS-".++$j, "C128",1,70)!!}&nbsp;&nbsp;</td>
+  
   </tr>
   @endfor
 </table>
